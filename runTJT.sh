9698bac65662a163f26d1f7d97b79f1f2e26fa0d
alecacco <ale.cacco@gmail.com>
2019-03-10 02:26:16 +0100

    added stript to manage logging
    Logging to file via bash script runTJT.sh

---
+++ runTJT.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#runs TJTuner automatically managing logging. First parameter is log file name, if set to "auto" will use a timestamp
+
+logprefix="log"
+if [ "$1" == 'auto'  ]
+    then
+        logprefix="`date +%Y-%m-%d_%H-%M-%S`"
+fi
+
+#In order to make tee work python works unbuffered. There is an issue with stderr, that's why a separate file is used, printed just because at the end of the execution
+python -u TJTuner.py ${@:2:99} 2>$logprefix.err | tee $logprefix.out
+
+cat $logprefix.err
